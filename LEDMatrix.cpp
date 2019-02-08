381a461d39e322ea2e49d638eb0d0d8fc4b32108
Zandwich <47357755+Zandwich@users.noreply.github.com>
2019-02-08 13:44:23 -0500

    Initial Project commit

---
+++ LEDMatrix.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "TLC5955.h"
+
+void setup() {
+
+Serial.begin(115200);
+
+}
+
+void loop() {
+
+
+}
---
+++ hardware.h
@@ -0,0 +1,15 @@
+/*
+ * hardware.h
+ *
+ *  Created on: 8 févr. 2019
+ *      Author: 200828272
+ */
+
+#ifndef HARDWARE_H_
+#define HARDWARE_H_
+
+
+
+
+
+#endif /* HARDWARE_H_ */
